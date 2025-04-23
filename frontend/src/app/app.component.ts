23f1cd6fe4f5a2281d32a20bd798ba127ded4542
Luka Krstikj <luka.krstik@protonmail.com>
2025-04-24 01:43:47 +0200

    signin signup components init commit, funkcionalni (dizajn) tailwind dodaden

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {SigninComponent} from "./signin/signin.component";
+import {SignupComponent} from "./signup/signup.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ SigninComponent, SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ frontend/src/app/signup/signup.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+  protected type: boolean = false;
+
+  showPassword(){
+    this.type = !this.type;
+  }
+}
