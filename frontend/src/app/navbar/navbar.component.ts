d4a75996388a837b1896bdf8a9f4f0590dad4c94
Luka Krstikj <luka.krstik@protonmail.com>
2025-04-27 14:00:38 +0200

    promeneto settings popup menito

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, HostListener, ViewChild} from '@angular/core';
 import {SettingsComponent} from "../settings/settings.component";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    SettingsComponent
+    SettingsComponent,
+    NgIf
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  @ViewChild("btn") private btn: ElementRef | undefined;
 
+  protected menuBool = false;
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    if (this.btn) {
+      let el = this.btn.nativeElement;
+      if (!el.contains(event.target)) {
+        this.menuBool = false; // Hide menu on outside click
+      }
+    }
+  }
+
+  toggleDropdown() {
+    this.menuBool = !this.menuBool;
+  }
 }
